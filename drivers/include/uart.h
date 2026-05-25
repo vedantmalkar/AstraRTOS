ed181cfc1ce6a4b303b6559813c7b9664b857768
mustafa <mustafataherramp@gmail.com>
2026-05-25 13:46:38 +0530

    drivers: implement interrupts in uart
    Signed-off-by: Mustafa Taher <mustafataherramp@gmail.com>

--- drivers/include/uart.h
+++ drivers/include/uart.h
@@ -9,13 +9,16 @@
 #define USART2_BRR (*(volatile uint32_t*)(USART2_BASE + 0x08))
 #define USART2_CR1 (*(volatile uint32_t*)(USART2_BASE + 0x0C))
 #define USART2_CR2 (*(volatile uint32_t*)(USART2_BASE + 0x10))
+#define NVIC_ISER1 (*(volatile uint32_t*)(0xE000E104))
+#define ICSR (*((volatile uint32_t *)0xE000ED04))
 
 #define TIMEOUT 1000
 
 void uart_init(void);
 void uart_send_char(char c);
 void uart_send_string(char *str);
-int uart_receive_char(void);
-void uart_receive_string(char *buffer, int max_length);
+int uart_receive_char_polling(void);
+void uart_receive_string_polling(char *buffer, int max_length);
+int uart_receive_byte(char *byte);
 
 #endif
