6eee8849d3705af49534bc53c99f4efcf37bf92e
Moksh Panicker <mokshpanicker.7@gmail.com>
2026-05-25 21:04:52 +0000

    kernel: address review comments
    Restore main.c, move timer demo to samples/software_timers/.
    
    Signed-off-by: Moksh <mokshpanicker.7@gmail.com>

--- app/main.c
+++ app/main.c
@@ -1,13 +1,8 @@
 // AstraRTOS - main kernel
 
-#include "timer.h"
-#include "task.h"
-
-void app_timer_demo_start(void);
-
 void main(void){
-    app_timer_demo_start();
-    os_start();
     while(1){
-        }
+
+
+    }
 }
